d69f720e7e767227981a6828a1e6616579dca74d
achooan <88soldieron@gmail.com>
2019-08-18 00:26:41 +0900

    Fix typo
    - change from pythonista to pythonistas

--- pycon_2019.py
+++ pycon_2019.py
@@ -1,8 +1,8 @@
 from art import tprint
 
-connect = f"{'32'*4}47323267427878696784{'32'*26}9210"
-the = f"60{'32'*8}847269{'32'*4}42{'32'*48}6210"
-pythonista = f"{'32'*6}92323280898472427873838465323247"
+connect = f"{'32'*4}4732323267427878696784{'32'*32}9210"
+the = f"3260{'32'*9}847269{'32'*4}42{'32'*54}6210"
+pythonistas = f"{'32'*6}923232808984724278738384658332323247"
 
-for word in (connect, the, pythonista):
+for word in (connect, the, pythonistas):
     tprint("".join(chr(int(word[i - 2 : i])) for i in range(2, len(word) + 1, 2)))
